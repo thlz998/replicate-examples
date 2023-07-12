fcd4003cd7009a07b49362facf4a8cc43e9ea7d2
k0a1a <thlz998@gmail.com>
2023-07-12 14:13:47 +0800

    update

--- qrcode_generation/run.sh
+++ qrcode_generation/run.sh
@@ -2,7 +2,7 @@
 
 cog predict \
 -i prompt="monkey scuba diving" \
--i qr_code_content="殷迦南殷迦南殷迦南" \
+-i qr_code_content="https://dl-1257240317.cos.ap-guangzhou.myqcloud.com/k0a1a/test-qrcode.png" \
 -i negative_prompt="ugly, disfigured, low quality, blurry, nsfw" \
 -i num_inference_steps=100 \
 -i guidance_scale=30 \
